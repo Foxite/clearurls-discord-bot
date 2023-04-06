9ce48f998575333a835b3cb4bdcb40672e90392c
Dirk Kok <the@dirkkok.nl>
2023-04-06 21:21:31 +0200

    fixed docker build

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from discord import Intents
 from dotenv import load_dotenv
 from unalix import clear_url
 
-delete_emoji = os.environ['DELETE_EMOJI']
-
 def clean_message(content):
     # Extract links and clean
     cleaned = ""
@@ -34,30 +32,37 @@ def clean_message(content):
     return any_cleaned, cleaned
 
 
-class MyClient(discord.Client):
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
-
-        any_cleaned, cleaned = clean_message(message.content)
-
-        # Send message and add reactions
-        if any_cleaned:
-            #text = 'It appears that you have sent one or more links with tracking parameters. Below are the same links with those fields removed:\n' + '\n'.join(cleaned)
-            message_files = []
-            for att in message.attachments:
-                message_files.append(await att.to_file())
-
-            await message.channel.send("Cleaned message by " + message.author.mention + ": (react with " + delete_emoji + " to delete)\n" + cleaned, mention_author=False, files=message_files, reference=message.reference)
-            await message.delete()
-
-    async def on_reaction_add(self, reaction, user):
-        if reaction.message.author == client.user and len(reaction.message.mentions) > 0 and reaction.message.mentions[0] == user and str(reaction.emoji) == delete_emoji:
-            await reaction.message.delete()
-
 if __name__ == "__main__":
     load_dotenv()
+    delete_emoji = os.environ['DELETE_EMOJI']
+    bot_token = os.environ['TOKEN']
+
+    class MyClient(discord.Client):
+        async def on_message(self, message):
+            if message.author == client.user:
+                return
+
+            any_cleaned, cleaned = clean_message(message.content)
+
+            # Send message and add reactions
+            if any_cleaned:
+                # text = 'It appears that you have sent one or more links with tracking parameters. Below are the same links with those fields removed:\n' + '\n'.join(cleaned)
+                message_files = []
+                for att in message.attachments:
+                    message_files.append(await att.to_file())
+
+                await message.channel.send(
+                    "Cleaned message by " + message.author.mention + ": (react with " + delete_emoji + " to delete)\n" + cleaned,
+                    mention_author=False, files=message_files, reference=message.reference)
+                await message.delete()
+
+        async def on_reaction_add(self, reaction, user):
+            if reaction.message.author == client.user and len(reaction.message.mentions) > 0 and \
+                    reaction.message.mentions[0] == user and str(reaction.emoji) == delete_emoji:
+                await reaction.message.delete()
+
+
     intents = Intents.default()
     intents.message_content = True
     client = MyClient(intents=intents)
-    client.run(os.environ['TOKEN'])
+    client.run(bot_token)
