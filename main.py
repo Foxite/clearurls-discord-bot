60ae486baf2de772400335539162c692a2ee752a
Dirk Kok <the@dirkkok.nl>
2023-04-06 20:06:45 +0200

    split business logic into function

--- main.py
+++ main.py
@@ -5,33 +5,39 @@ from discord import Intents
 from dotenv import load_dotenv
 from unalix import clear_url
 
+def clean_message(content):
+    # Extract links and clean
+    cleaned = ""
+    any_cleaned = False
+    last_match = None
+    for match in re.finditer('(?P<url>https?://[^\s]+)', content):
+        if last_match is None:
+            cleaned += content[0:match.start()]
+        else:
+            cleaned += content[last_match.end():match.start()]
+
+        last_match = match
+
+        url = content[match.start():match.end()]
+        cleaned_url = clear_url(url)
+        if cleaned_url != url:
+            cleaned += cleaned_url
+            any_cleaned = True
+        else:
+            cleaned += url
+
+    if last_match is not None:
+        cleaned += content[last_match.end():]
+
+    return any_cleaned, cleaned
+
+
 class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author == client.user:
             return
 
-        # Extract links and clean
-        cleaned = ""
-        any_cleaned = False
-        last_match = None
-        for match in re.finditer('(?P<url>https?://[^\s]+)', message.content):
-            if last_match is None:
-                cleaned += message.content[0:match.start()]
-            else:
-                cleaned += message.content[last_match.end():match.start()]
-
-            last_match = match
-
-            url = message.content[match.start():match.end()]
-            cleaned_url = clear_url(url)
-            if cleaned_url != url:
-                cleaned += cleaned_url
-                any_cleaned = True
-            else:
-                cleaned += url
-
-        if last_match is not None:
-            cleaned += message.content[last_match.end():]
+        any_cleaned, cleaned = clean_message(message.content)
 
         # Send message and add reactions
         if any_cleaned:
@@ -39,8 +45,9 @@ class MyClient(discord.Client):
             await message.reply(cleaned, mention_author=False)
             await message.delete()
 
-load_dotenv()
-intents = Intents.default()
-intents.message_content = True
-client = MyClient(intents=intents)
-client.run(os.environ['TOKEN'])
+if __name__ == "__main__":
+    load_dotenv()
+    intents = Intents.default()
+    intents.message_content = True
+    client = MyClient(intents=intents)
+    client.run(os.environ['TOKEN'])
